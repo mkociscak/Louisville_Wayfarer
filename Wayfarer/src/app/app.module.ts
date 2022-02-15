4fa58e4c7003edb9582573157b00ab3a8dc5362a
Brian <brian.1980.habeeb@gmail.com>
2022-02-15 16:45:22 -0500

    components added

--- Wayfarer/src/app/app.module.ts
+++ Wayfarer/src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { PostsComponent } from './posts/posts.component';
+import { NavComponent } from './nav/nav.component';
+import { CitylistComponent } from './citylist/citylist.component';
+import { SlideshowComponent } from './slideshow/slideshow.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PostsComponent,
+    NavComponent,
+    CitylistComponent,
+    SlideshowComponent
   ],
   imports: [
     BrowserModule,
