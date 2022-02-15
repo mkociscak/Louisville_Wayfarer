32cfaf0e9eece1b73bce6de4da586bfc928de4a0
jeremy <jeremywalker1216@gmail.com>
2022-02-15 16:48:10 -0500

    began city object list

---
+++ Wayfarer/src/app/cities/cityobjects.ts
@@ -0,0 +1,25 @@
+export const CITY = [
+
+{
+    id: 1,
+    name: 'San Francisco',
+    //img:
+    posts: []
+},
+
+{
+    id: 2,
+    name: 'London',
+    //img:
+    posts: []
+}, 
+{
+    id: 3,
+    name: 'Gibraltar',
+    //img
+    posts: []
+}
+
+
+
+]
