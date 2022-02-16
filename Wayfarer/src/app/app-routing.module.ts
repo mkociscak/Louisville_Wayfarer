64ecb75059b0fe216f09033b3f078eaf1891661a
Noah Dickerson <Noah.dickerson00@gmail.com>
2022-02-16 13:04:15 -0500

    pictures and buttons on home start

--- Wayfarer/src/app/app-routing.module.ts
+++ Wayfarer/src/app/app-routing.module.ts
@@ -1,9 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { SlideshowComponent } from './slideshow/slideshow.component';
+
 
 import { CitiesComponent } from './cities/cities.component';
 
-const routes: Routes = [
+const routes: Routes = [ {
+  path: 'home',
+  component: SlideshowComponent
+}
   
 ];
 
--- Wayfarer/src/app/app.module.ts
+++ Wayfarer/src/app/app.module.ts
@@ -5,12 +5,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { CitiesComponent } from './cities/cities.component';
 import { NavComponent } from './nav/nav.component';
+import { SlideshowComponent } from './slideshow/slideshow.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     CitiesComponent,
-    NavComponent
+    NavComponent,
+    SlideshowComponent
   ],
   imports: [
     BrowserModule,
