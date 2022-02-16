2b5cb4330b5596bc18bd76de80592c1f6dbddeb1
Noah Dickerson <Noah.dickerson00@gmail.com>
2022-02-16 11:39:37 -0500

    Navbar is in Nav Module

--- Wayfarer/src/app/app.module.ts
+++ Wayfarer/src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { CitiesComponent } from './cities/cities.component';
+import { NavComponent } from './nav/nav.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    CitiesComponent
+    CitiesComponent,
+    NavComponent
   ],
   imports: [
     BrowserModule,
