71dd18abb185d631e6e55d81a5f71ee4b544c5f8
Brian <brian.1980.habeeb@gmail.com>
2022-02-18 11:14:44 -0500

    posts added to SanFran

--- Wayfarer/src/app/cityobjects.ts
+++ Wayfarer/src/app/cityobjects.ts
@@ -2,18 +2,37 @@ export const CITY = [
 
 {
     id: 1,
-    name: 'San Francisco'
+    name: 'San Francisco',
+    author: 'Dave Bergman',
+    postId: 1,
+    title: 'Golden Gate Bridge',
+    body:  'Golden Gate Bridge: For any one traveling to San Fran a must see is the GGB. We took the "30" minute ride (plan on CA traffic) which took a little under 2 hours and cost $150 (should have rented bikes) to see the world renowned landmark.  Unfortunately, another famous San Fran landmark, the fog, made it nearly impossible to see the 1.7 mi, 750 ft engineering marvel.  Advice to those traveling to SF listen to the infamous quote and stay away during the summer.  However, we did travel north across the bridge to the Golden Gate Bridge national recreation area and were rewarded with an amazing view of the city as the fog cleared quicker than you can say SF GGB.  After snapping iconic photo after photo, we headed north into the Redwood Forrest to camp and enjoy nature.  Follow my travel blog for a review of our campsite. ***'},
+        
+{   
+    id: 1,
+    name: 'San Francisco', 
+    author: "Skip Caray",
+    postId: 2,
+    title: "Oracle Park",
+    body: "Oracle Park:  As a life-long fan of the game, I decided to take my dad, Harry, on a road trip across America, visiting ballparks along the way.  Our final destination was Oracle Park, in beautiful San Fran.  The views of the bay made the $16 16oz pints completely worth the cost.  Although the Giants beat our hometeam, the Cubs, we were impressed with the modern park and it's amenities.  Individual stalls in the bathrooms, unlike the troughs back home on the Northside, made us feel like we were actually in the 21st centuary.  But beyond that, the Garden, which serves to inspire and educate fans of all ages about sustainability, urban farming, and healthy eating truly blew us away. The two bistros located inside the Garden, Hearth Table and Garden Table, offer a healthy menu featuring fruits and vegetables picked fresh from the Garden. We invite you to explore the aeroponic towers and garden beds in this one-of-a-kind space and enjoy garden-fresh meals at the bistros and garden-inspired beverages at the bar.  Although the game of baseball has been a family friendly outing for well over a century, the Garden proves to carry that tradition well into the future.  ****"},
+{  
+    id: 1,
+    name: 'San Francisco',
+    author: 'Julia Child',
+    postId: 3,
+    title: 'Ghirardelli Square',
+    body: "Ghirardelli Square:  Along the San Fran waterfront lies the food destination of Ghirardelli Square.  Among the many restaurants that encompass the square there are other attractions, such as, retail stores, activities for all ages, the Ghirardelli Chocolate Factory, San Francisco Maritime National Historical Park, and Fisherman's Wharf.  All that being said, I came for the food!  As a huge chocolate lover, the tour of the factory made me feel like I was in candyland, literally.  The shear scale of production made me feel small and at the same time, better about myself knowing that I was not alone in my obsession for this decandant, delicious, delicacy, dessert.  After I had walked for what felt like miles, I realized I was hungry for more than sweets.  So I popped in to Barrio Tacos and Tequila for more than tacos.  The fresh salsa and margs were a great start to the meal.  However, the churros dipped in warm Ghirardelli's dark chocolate sauce spiced with a little cayanne was the perfect end to a scrumptious meal, as well as, a shot of Don Julio 1942 for good measure!  I'll be back.  ***"
     
 },
 
-{
-    id: 2,
-    name: 'London'
+// {
+//     id: 2,
+//     name: 'London'
     
-}, 
-{
-    id: 3,
-    name: 'Gibraltar'
-}
+// }, 
+// {
+//     id: 3,
+//     name: 'Gibraltar'
+// }
   
 ]
